88384c1f7e7303d1f5f19173c87775bbd678e8bf
0x0FACED <https.genius@gmail.com>
2024-06-28 11:09:10 +0300

    upd: impl server struct, added New(), Start() and prepare() funcs

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,34 @@
 package server
 
+import (
+	"time-tracker/configs"
+	"time-tracker/internal/storage"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Server struct {
+	cfg configs.ServerConfig
+	r   *gin.Engine
+	db  storage.Database
 }
 
-func (s *Server) prepare() error {
-	// TODO: impl
-	panic("not impl")
+func New(cfg configs.ServerConfig, db storage.Database) *Server {
+	return &Server{
+		r:   gin.Default(),
+		cfg: cfg,
+		db:  db,
+	}
+}
+
+func (s *Server) Start() error {
+	s.prepare()
+	addr := s.cfg.Host + ":" + s.cfg.Port
+	s.r.Run(addr)
+	return nil
+}
+
+func (s *Server) prepare() {
+	s.prepareRoutes()
+	// ... //
 }
