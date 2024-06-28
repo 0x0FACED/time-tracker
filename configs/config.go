5091399a873332c6205b78b17ce05fee5982c458
0x0FACED <https.genius@gmail.com>
2024-06-28 11:07:34 +0300

    upd: added Load() func to load env config

--- configs/config.go
+++ configs/config.go
@@ -1,5 +1,11 @@
 package configs
 
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
 type DatabaseConfig struct {
 	DBUsername string
 	DBName     string
@@ -17,4 +23,24 @@ type Config struct {
 	DatabaseConfig
 	ServerConfig
 }
+
+func Load() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		DatabaseConfig{
+			DBUsername: os.Getenv("DB_USER"),
+			DBPass:     os.Getenv("DB_PASS"),
+			DBName:     os.Getenv("DB_NAME"),
+			DBHost:     os.Getenv("DB_HOST"),
+			DBPort:     os.Getenv("DB_PORT"),
+		},
+		ServerConfig{
+			Host: os.Getenv("S_HOST"),
+			Port: os.Getenv("S_PORT"),
+		},
+	}, nil
 }
