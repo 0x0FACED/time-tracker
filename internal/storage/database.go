d8f0175237f9803dc3a288b34358875192f7cb68
0x0FACED <https.genius@gmail.com>
2024-06-29 11:25:06 +0300

    added: user, task interfaces

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -1,6 +1,24 @@
 package storage
 
+import "time-tracker/internal/models"
+
 type Database interface {
 	Connect() error
 	Disconnect() error
+
+	UserDatabase
+	TaskDatabase
+}
+
+type UserDatabase interface {
+	GetUsers() (map[int]models.User, error)
+	GetTasksByUserID(id int) ([]models.Task, error)
+	AddUser(u *models.User) error
+	DeleteUser(id int) error
+	UpdateUser(u *models.User) error
+}
+
+type TaskDatabase interface {
+	AddStartTask(t *models.Task) error
+	AddEndTask(t *models.Task) error
 }
