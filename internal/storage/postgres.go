9e97869665f639b99020d985b191f5169080cf52
0x0FACED <https.genius@gmail.com>
2024-06-30 23:09:55 +0300

    impl: AddStartTAck(), AddEndTask() and GetUserByID()

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"time"
 	"time-tracker/configs"
 	"time-tracker/internal/models"
 	"time-tracker/internal/utils/queries"
@@ -75,6 +76,17 @@ func (p *Postgres) GetUsers() (map[int]models.User, error) {
 	return users, nil
 }
 
+func (p *Postgres) GetUserByID(id int) (*models.User, error) {
+	var user models.User
+	query := "SELECT id, passport_number, pass_serie, surname, name, patronymic, address FROM users WHERE id = $1"
+	row := p.sql.QueryRow(query, id)
+	err := row.Scan(&user.Id, &user.PassNumber, &user.PassSerie, &user.Surname, &user.Name, &user.Patronymic, &user.Address)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
 func (p *Postgres) GetTasksByUserID(id int) ([]models.Task, error) {
 	// TODO: impl
 	panic("not impl")
@@ -104,11 +116,19 @@ func (p *Postgres) UpdateUser(u *models.User) error {
 }
 
 func (p *Postgres) AddStartTask(t *models.Task) error {
-	// TODO: impl
-	panic("not impl")
+	now := time.Now()
+	_, err := p.sql.Exec("INSERT INTO tasks (user_id, description, start_time) VALUES ($1, $2, $3)", t.UserID, t.Desc, now)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (p *Postgres) AddEndTask(t *models.Task) error {
-	// TODO: impl
-	panic("not impl")
+func (p *Postgres) AddEndTask(id int) error {
+	now := time.Now()
+	_, err := p.sql.Exec("UPDATE tasks SET end_time = $1 WHERE id = $2", now, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
