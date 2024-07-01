6727816c9d2c0fecffcee9ca837b10ebb23e64b7
0x0FACED <https.genius@gmail.com>
2024-07-01 12:06:31 +0300

    added: getUsersReq for filtering and pagination

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,10 +2,21 @@ package models
 
 type User struct {
 	Id         int    `json:"id" db:"id"`
-	PassNumber int    `json:"pass_number" db:"pass_number"`
-	PassSerie  int    `json:"pass_serie" db:"pass_serie"`
+	PassNumber string `json:"passport_number" db:"passport_number"`
+	PassSerie  string `json:"pass_serie" db:"pass_serie"`
 	Surname    string `json:"surname" db:"surname"`
 	Name       string `json:"name" db:"name"`
 	Patronymic string `json:"patronymic" db:"patronymic"`
 	Address    string `json:"address" db:"address"`
+}
+
+type GetUsersRequest struct {
+	PassportNumber string `form:"passport_number"`
+	PassSerie      string `form:"pass_serie"`
+	Surname        string `form:"surname"`
+	Name           string `form:"name"`
+	Patronymic     string `form:"patronymic"`
+	Address        string `form:"address"`
+	Page           int    `form:"page" binding:"required"`
+	PageSize       int    `form:"page_size" binding:"required"`
 }
