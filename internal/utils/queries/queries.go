40d4dba8ce864d3715d007758c0f2e027def8ec7
0x0FACED <https.genius@gmail.com>
2024-07-01 12:11:23 +0300

    was: surname, name; now: name, surname

--- internal/utils/queries/queries.go
+++ internal/utils/queries/queries.go
@@ -1,9 +1,9 @@
 package queries
 
 const (
-	GetUsers = `SELECT id, passport_number, pass_serie, surname, name, patronymic, address FROM users`
+	GetUsers = `SELECT id, passport_number, pass_serie, name, surname, patronymic, address FROM users`
 
-	AddUser = `INSERT INTO users (pass_number, pass_serie, surname, name, patronymic, address) VALUES ($1, $2, $3, $4, $5, $6)`
+	AddUser = `INSERT INTO users (passport_number, pass_serie, name, surname, patronymic, address) VALUES ($1, $2, $3, $4, $5, $6)`
 
 	DeleteUser = `DELETE FROM users WHERE id = $1`
 )
