1d7f38b27f254c22f5917b0251fef471093dd00f
0x0FACED <https.genius@gmail.com>
2024-07-01 12:37:34 +0300

    added: worklog struct and getUserLogsReq struct

--- internal/models/task.go
+++ internal/models/task.go
@@ -9,3 +9,15 @@ type Task struct {
 	EndTime   time.Time `json:"end_time" db:"end_time"`
 	Desc      string    `json:"description" db:"description"`
 }
+
+type Worklog struct {
+	TaskID  int     `json:"task_id"`
+	Hours   float64 `json:"hours"`
+	Minutes float64 `json:"minutes"`
+}
+
+type GetUserWorklogsRequest struct {
+	UserID    int       `form:"user_id" binding:"required"`
+	StartDate time.Time `form:"start_date" binding:"required"`
+	EndDate   time.Time `form:"end_date" binding:"required"`
+}
