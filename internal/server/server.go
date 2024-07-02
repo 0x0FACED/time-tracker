5fc42e6a25497cc54c20a4fcf50cd5f9a0a825d6
0x0FACED <https.genius@gmail.com>
2024-07-02 15:35:05 +0300

    added: logger field to server struct

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,31 +1,33 @@
 package server
 
 import (
-	"log"
 	"time-tracker/configs"
 	"time-tracker/internal/storage"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type Server struct {
-	cfg configs.ServerConfig
-	r   *gin.Engine
-	db  storage.Database
+	cfg    configs.ServerConfig
+	r      *gin.Engine
+	db     storage.Database
+	logger *zap.SugaredLogger
 }
 
-func New(cfg configs.ServerConfig, db storage.Database) *Server {
+func New(cfg configs.ServerConfig, db storage.Database, log *zap.SugaredLogger) *Server {
 	return &Server{
-		r:   gin.Default(),
-		cfg: cfg,
-		db:  db,
+		r:      gin.Default(),
+		cfg:    cfg,
+		db:     db,
+		logger: log,
 	}
 }
 
 func (s *Server) Start() error {
 	s.prepare()
 	addr := s.cfg.Host + ":" + s.cfg.Port
-	log.Println("ADDR: ", addr)
+	s.logger.Infow("server ready to start", "addr", addr)
 	s.r.Run(addr)
 	return nil
 }
