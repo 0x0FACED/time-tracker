d0d6b50de7929551c984d4dd70059cd766090ef5
0x0FACED <https.genius@gmail.com>
2024-07-02 15:33:36 +0300

    added: zap init and logging

--- cmd/run.go
+++ cmd/run.go
@@ -1,29 +1,55 @@
 package cmd
 
 import (
-	"log"
 	"time-tracker/configs"
 	"time-tracker/internal/server"
 	"time-tracker/internal/storage"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func Run() {
+	config := zap.NewProductionConfig()
+	config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	config.OutputPaths = []string{"stdout", "./logs.txt"}
+
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.TimeKey = "timestamp"
+	config.EncoderConfig.MessageKey = "message"
+	config.EncoderConfig.LevelKey = "level"
+	config.EncoderConfig.CallerKey = "caller"
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
+
 	cfg, err := configs.Load()
 	if err != nil {
-		log.Fatalln("cant load cfg file: ", err)
+		sugar.Fatalln("cant load config, error: ", err)
 		return
 	}
-	log.Println("CONFIG: ", cfg)
+	sugar.Infow("config loaded",
+		"config", cfg,
+	)
 	db := storage.New(cfg.DatabaseConfig)
-	log.Println("DB: ", db)
+
 	err = db.Connect()
 	if err != nil {
-		log.Fatalln("cant Connect() to db: ", err)
+		sugar.Fatalln("cant connect to db, error: ", err)
+		return
 	}
-	s := server.New(cfg.ServerConfig, db)
+	sugar.Infoln("successfully connected to db")
+	s := server.New(cfg.ServerConfig, db, sugar)
+	sugar.Infoln("starting the server")
 	err = s.Start()
 	if err != nil {
-		log.Fatalln("cant start the server: ", err)
+		sugar.Fatalw("cant start the server",
+			"error", err,
+		)
 		return
 	}
 }
